1975afaec4161c9bacb163c730957816c54a7e05
GabrielSOliv <100054103+GabrielSOliv@users.noreply.github.com>
2024-08-31 12:38:20 -0300

    solve with two pointers

--- Medium/Reverse Integer - Leet Code/solve.py
+++ Medium/Reverse Integer - Leet Code/solve.py
@@ -2,18 +2,20 @@ example1 = 1534236469
 
 
 def reverse(x: int) -> int:
-        strInput = str(x)
-        strReversed = ''
-        for i in range(len(strInput) -1 , -1, -1):
-            if strInput[i] == '-':
-                strReversed = '-' + strReversed
-            else:
-                strReversed += strInput[i]
-        
-        if int(strReversed) <= -2**31 or int(strReversed) >= 2**31 - 1:
-            strReversed = '0'
+        num = list(str(abs(x)))
+        left, right = 0, len(num) - 1
 
-        return int(strReversed)
+        while left < right:
+            num[left], num[right] = num[right], num[left]
+            left, right = left + 1, right - 1
+
+        if int(''.join(num)) > 2**31:
+            return 0
+
+        if x < 0:
+            return int(''.join(num)) * (-1)
+
+        return int(''.join(num))
     
     
 print(reverse(example1))
